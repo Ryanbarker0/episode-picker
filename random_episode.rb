37ccc34977dd97cb08bda243961c417f21df4a9a
Ryan <ryangeorgebarker0@gmail.com>
2018-09-23 17:24:33 +0100

    opens episodes at random now

--- random_episode.rb
+++ random_episode.rb
@@ -1,39 +1,32 @@
-require 'pry'
+#!/bin/ruby
+
 # this function runs in terminal
 # it picks a season and episode at random
 # then returns the result as a string
 
-# def episode_picker
-#   season = rand(3..9)
-#   episode = rand(1..23)
-#   puts "Season: #{season} Episode: #{episode}"
-# end
-
 def random_num_generator
-  first_num = rand(1..1)
-  second_num = rand(3..3)
+  first_num = rand(2..9) #NOTE: generates a random number that is firt name and last name. s
+  second_num = rand(1..25)
   second_num < 10 ? "0#{first_num}0#{second_num}" : "0#{first_num}#{second_num}"
+  #this returns value that, for example is 0101, relative to whether or not there is a requirement for the additional "0"
+  #value is equal to a string
 end
 
-def video_file_iterate
+#NOTE: terminal is outputting You are Watching, multiple times, but only opens 1 episode.
+def episode_picker
+  Dir.foreach("/Users/Ryan/Documents/GitHub/videos") do |x|
+    next if x == "." or x == ".."
+    if x == "#{random_num_generator}.mkv"
+      system %{open "/Users/Ryan/Documents/GitHub/videos/#{x}"}
+    elsif x == "#{random_num_generator}.mp4"
+      puts "You are watching Season: #{x[0..1]} Episode: #{x[2..3]}!"
+      system %{open "/Users/Ryan/Documents/GitHub/videos/#{x}"}
+    elsif x == "#{random_num_generator}.avi"
+      puts "You are watching Season: #{x[0..1]} Episode: #{x[2..3]}!"
+      system %{open "/Users/Ryan/Documents/GitHub/videos/#{x}"}
 
-  Dir.foreach("/Users/Ryan/Documents/GitHub/episode-picker/videos") do |item|
-    next if item == '.' or item == '..'
-    return "#{item[0..3]}"
+    end
   end
 end
 
-def episode_selector
-  path = "/Users/Ryan/Documents/GitHub/episode-picker/videos/"
-  if random_num_generator == video_file_iterate #NOTE: in theory, video_file_iterate keeps looping if it is not equal
-                                                #i don't think this is happening, need to look into how we iterate
-    system %{open "#{path}#{video_file_iterate}.mkv"}
-  end
-end
-
-binding.pry
-
-p 'end'
-
-
-# # episode_picker
+episode_picker
